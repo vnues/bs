1b99798a383dafd9ed776baa551daa647254acdf
wanggangfeng <wanggangfeng@forchange.tech>
2019-04-23 17:05:16 +0800

    修复数据库

--- app.js
+++ app.js
@@ -1,39 +1,40 @@
-const express = require('express')
-const path = require('path')
-const cookieParser = require('cookie-parser')
-const app = express()
-const fs = require('fs')
-var logger = require('morgan');
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const app = express();
+const fs = require("fs");
+var logger = require("morgan");
 
 //加载数据库
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-const bodyParser = require('body-parser')
+const bodyParser = require("body-parser");
 
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'ejs') //设置模板引擎
-app.use(cookieParser())
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({
-	extended: false
-}))
-app.use(express.static(path.resolve(__dirname, './dist')))
-app.use('/public', express.static(path.resolve(__dirname + '/public')))
-app.use('/uploads', express.static(path.resolve(__dirname + '/uploads')))
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs"); //设置模板引擎
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: false
+  })
+);
+app.use(express.static(path.resolve(__dirname, "./dist")));
+app.use("/public", express.static(path.resolve(__dirname + "/public")));
+app.use("/uploads", express.static(path.resolve(__dirname + "/uploads")));
 
 //打印属性
 // app.use(logger('dev'));
 
-app.use('/api', require('./routers/api'))
-app.use('/admin', require('./routers/admin'))
-app.use('/', require('./routers/main'))
-global.db = mongoose.connect('mongodb://localhost:27017/blog', (err) => {
-	if (err) {
-		console.log('数据库连接失败')
-	} else {
-		console.log('数据库连接成功')
-	}
-})
-
+app.use("/api", require("./routers/api"));
+app.use("/admin", require("./routers/admin"));
+app.use("/", require("./routers/main"));
+global.db = mongoose.connect("mongodb://localhost:27017/bs", err => {
+  if (err) {
+    console.log("数据库连接失败");
+  } else {
+    console.log("数据库连接成功");
+  }
+});
 
 module.exports = app;
